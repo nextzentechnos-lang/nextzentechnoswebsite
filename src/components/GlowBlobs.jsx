48bed7fcd1c419cd1080a2adf3be774e47d3e273
goutham2442-star <gouthamgagan2442@gmail.com>
2026-08-11 21:42:55 +0530

    feat: implement GlowBlobs background mesh blur circles

---
+++ src/components/GlowBlobs.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+export default function GlowBlobs() {
+  return (
+    <div className="glow-blobs-container">
+      <div className="glow-blob blob-orange"></div>
+      <div className="glow-blob blob-purple"></div>
+      <div className="glow-blob blob-cyan"></div>
+    </div>
+  );
+}
