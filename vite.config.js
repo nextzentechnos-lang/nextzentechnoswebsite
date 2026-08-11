4d58dd98fbbebad1fdc3dc0a6531096d7bcf773b
goutham2442-star <gouthamgagan2442@gmail.com>
2026-08-11 22:33:38 +0530

    build: configure build target for maximum browser compatibility and fix media query range issues

--- vite.config.js
+++ vite.config.js
@@ -4,4 +4,8 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  build: {
+    cssMinify: 'esbuild',
+    target: ['chrome80', 'safari13.1', 'firefox78', 'edge80', 'ios13']
+  }
 })
