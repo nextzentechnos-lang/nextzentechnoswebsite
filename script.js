054aa3c4c7763f0434f0b922f6e69d1ac99bab18
goutham2442-star <gouthamgagan2442@gmail.com>
2026-08-11 21:30:32 +0530

    feat: implement/update .gitignore

--- script.js
+++
@@ -1,6 +0,0 @@
-const toggle = document.querySelector(".menu-toggle");
-const nav = document.querySelector(".nav-links");
-
-toggle.addEventListener("click", () => {
-    nav.classList.toggle("active");
-});
